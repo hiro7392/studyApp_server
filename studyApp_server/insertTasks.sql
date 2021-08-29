3ceda5e83bb9337cdc67afd94bc5243bb6e057e3
川原大宙 <hiromichi@kawaharadaichuunoMacBook-Air-2.local>
2021-08-29 11:33:23 +0900

    0827のデモversion

--- studyApp_server/insertTasks.sql
+++
@@ -1,1 +0,0 @@
-insert into tasks(student_id,task_class,name,content,created_at,updated_at,deadline,achivement) value(1,1,"現代文","教科書p17~p64まで",now(),now(),now(),30);
--- studyApp_server/setDB.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS posts;
-
-CREATE TABLE posts (
-id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-author VARCHAR(100) NOT NULL,
-content VARCHAR(100) NOT NULL,
-PRIMARY KEY(id)
-);
-insert into posts(id,author,content) values(1,"hiromichi","hello_Obon!"),(2,"yamaguchi","GoodBye"),(3,"Arnold_Schwarzenegger","Goodnight");
